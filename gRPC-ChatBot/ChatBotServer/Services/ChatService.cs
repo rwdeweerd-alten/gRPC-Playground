8b1bdbbbea0c2bf582c7c64da161ecc0a2f4a888
Robin de Weerd <robin.de.weerd@alten.nl>
2022-09-12 21:20:17 +0200

    Step 5 - Implement Chat service in ChatBotServer

---
+++ gRPC-ChatBot/ChatBotServer/Services/ChatService.cs
@@ -0,0 +1,32 @@
+﻿using ChatBot.gRPC;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBotServer.Services
+{
+    public class ChatService : ChatBot.gRPC.Chat.ChatBase
+    {
+        private readonly ILogger _logger;
+
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task ChitChat(IAsyncStreamReader<ChatMessage> requestStream, IServerStreamWriter<ChatMessage> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current.Message;
+                _logger.LogInformation($"Received ChitChat message: {message}");
+
+                await responseStream.WriteAsync(
+                    new ChatMessage() { Message = $"Send response: {message}" });
+            }
+        }
+    }
+}
--- gRPC-ChatBot/ChatBotServer/Startup.cs
+++ gRPC-ChatBot/ChatBotServer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ChatBotServer.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +33,7 @@ namespace ChatBotServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ChatService>();
 
                 endpoints.MapGet("/", async context =>
                 {
